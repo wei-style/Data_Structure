4fe0e919d03d6e904b8572bff7bbb84af083040e
WangWei <138451059+wei-style@users.noreply.github.com>
2023-10-27 21:35:04 +0800

    Update test.c
    对顺序表的元素删除、求两个表的并集以及删除重复元素进行了测试

--- test.c
+++ test.c
@@ -1,9 +1,15 @@
 #define NULL ((void *)0)
 #include "sqlist.h"
 void test_insert();
+void test_delete();
+void test_merge();
+void test_purge();
 int main()
 {
-	test_insert();
+	//test_insert();
+	//test_delete();
+	//test_merge();
+	test_purge();
 	return 0;
 }
 
@@ -18,8 +24,77 @@ void test_insert()
 	int i = 0;
 	for (i = 0; i < N / 2; i++)
 	{
-		insert(L, N / 2 - i - 1, 0);
+		list_insert(L, N / 2 - i - 1, 0);
 	}
-	list_free(L);
 
+}
+void test_delete()
+{
+	sqlink L;
+	L = list_create();
+	if (L == NULL)
+	{
+		return;
+	}
+	list_insert(L, 10, 0);
+	list_insert(L, 20, 0);
+	list_insert(L, 30, 0);
+	list_insert(L, 40, 0);
+	list_insert(L, 50, 0);
+	list_insert(L, 60, 0);
+	list_show(L);
+	list_delete(L, 1);
+	list_show(L);
+
+}
+void test_merge()
+{
+	sqlink L1, L2;
+	L1 = list_create();
+	L2 = list_create();
+
+	if (L1 == NULL)
+	{
+		return;
+	}
+
+	if (L2 == NULL)
+	{
+		return;
+	}
+	list_insert(L1, 10, 0);
+	list_insert(L1, 20, 0);
+	list_insert(L1, 30, 0);
+	list_insert(L1, 40, 0);
+
+	list_insert(L2, 50, 0);
+	list_insert(L2, 20, 0);
+	list_insert(L2, 50, 0);
+	list_insert(L2, 40, 0);
+	list_show(L1);
+	list_show(L2);
+	printf("******************\n");
+	list_merge(L1, L2);
+	list_show(L1);
+	list_show(L2);
+
+}
+void test_purge()
+{
+	sqlink L;
+	L = list_create();
+	if (L == NULL)
+	{
+		return;
+	}
+	list_insert(L, 10, 0);
+	list_insert(L, 10, 0);
+	list_insert(L, 10, 0);
+	list_insert(L, 10, 0);
+	list_insert(L, 10, 0);
+	list_insert(L, 10, 0);
+	list_show(L);
+	list_purge(L);
+	list_show(L);
+	return 0;
 }
