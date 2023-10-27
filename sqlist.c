7f8b614717bec1977d49498e9fb107039d7d506b
WangWei <138451059+wei-style@users.noreply.github.com>
2023-10-27 21:33:34 +0800

    Update sqlist.c
    2023年10月27日，今天学习了顺序表的元素删除、求两个表的并集与删除重复元素的操作，以此记录

--- sqlist.c
+++ sqlist.c
@@ -55,8 +55,14 @@ int list_length(sqlink L)
 }
 //定位表中元素
 int locate(sqlink L, data_t value)
-{
-	return 0;
+{//将每个元素都与输入参数做对比
+	int i = 0;
+	for (i = 0; i <= L->last; i++)
+	{
+		if (L->data[i] == value)
+			return i;//如果在表中找到这个数，就返回他的位置，如果有多个元素和他相同，返回第一个位置就行
+	}
+	return -1;
 }
 //插入
 int insert(sqlink L, data_t value, int pos)
@@ -115,5 +121,82 @@ int list_free(sqlink L)
 	free(L);
 	L = NULL;//释放内存后L仍然指向这块空间，令其等于NULL可以避免后续再对其进行访问
 	printf("free list done\n");
+	return 0;
+}
+//删除表中元素
+int list_delete(sqlink L, int pos)
+{	//判断是否是空表
+	if(L->last==-1)
+	{
+		printf("list is empty\n");
+		return -1;
+	}
+	//检查这个删除的位置是否满足表中的有效数据位
+	if (pos<0 || pos>L->last)
+	{
+		printf("delete is invalid\n");
+		return -1;
+	}
+	//删除元素后面的元素要向前移动
+	int i;
+	for(i=pos+1;i<=L->last;i++)
+	{
+		L->data[i-1] = L->data[i];//data[pos]的值自动的被覆盖
+	}
+	//更新表的成员数
+	L->last--;
+	return 0;
+}
+//求两个表的并集
+int list_merge(sqlink L1, sqlink L2)
+{
+	int ret;
+	//依次取L2中的元素
+	int i = 0;
+	while (i <= L2->last)
+	{
+		//检查取出的元素是否在L1中（循环进行）
+		ret=list_locate(L1, L2->data[i]);//判断L2中的元素在不在L1中
+		//如果不在L1中就将其插入L1（循环进行）
+		if (ret == -1)//没找到,也就是两个表中有不同的元素
+		{
+			if(list_insert(L1, L2->data[i], L1->last+1)==-1)//将不同的元素插入L1的最后面，如果插入失败会返回-1，表示表已经满了 
+			{
+				printf("list %s is full\n", L1);
+				return -1;
+			}
+		}
+		i++;
+	}
+	return 0;
+}
+//删除线性表中的重复元素
+int list_purge(sqlink L)
+{
+	int i = 1;
+	if (L->last == 0)
+		return 0;
+	int j;
+	//一次取元素i，i从1开始到last，判断
+	while (i <= L->last)
+	{
+		j = i - 1;
+		while(j>=0)
+		{
+			if (L->data[i] == L->data[j])
+			{
+				list_delete(L, i);
+				break;
+			}
+			else
+			{
+				j--;
+			}
+		}
+		if(j<0)
+		{
+			i++;
+		}
+	}
 	return 0;
 }
