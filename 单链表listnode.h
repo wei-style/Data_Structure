9a905d091ea015953677b933b4cfd7a449a76f43
WangWei <138451059+wei-style@users.noreply.github.com>
2023-10-30 22:02:20 +0800

    Update 单链表listnode.h

--- 单链表listnode.h
+++ 单链表listnode.h
@@ -14,3 +14,6 @@ typedef struct node* linklist;
 linklist list_create(void);
 int list_tail_insert(linklist H, data_t value);//head头节点
 int list_show(linklist H);
+linklist list_get(linklist H, int pos);//找到某个位置的前一个结点
+int list_insert(linklist H, data_t value, int pos);
+linklist list_delete(linklist H,int pos);
