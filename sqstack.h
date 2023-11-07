69aa506a944075112994b1e4d6ea24d420600ece
WangWei <138451059+wei-style@users.noreply.github.com>
2023-11-07 14:24:17 +0800

    Create sqstack.h

---
+++ sqstack.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+typedef data_t;
+typedef struct
+{
+	data_t* data;
+	int maxlen;
+	int top;
+}sqstack;
+
+
+sqstack* stack_create(int len);
+int stack_push(sqstack* s, data_t value);
+int stack_empty(sqstack* s);
+int stack_full(sqstack* s);
+data_t stack_pop(sqstack* s);
+data_t stack_top(sqstack* s);
+int stack_clear(sqstack* s);
+int stack_free(sqstack* s);
